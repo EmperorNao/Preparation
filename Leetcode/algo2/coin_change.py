415d3327e99cf1bd6f4ff4be73640c7266d2d029
Vladyslav Fakhretdinov <emperornao@yandex-team.ru>
2022-04-12 17:39:25 +0300

    solved

--- Leetcode/algo2/coin_change.py
+++ Leetcode/algo2/coin_change.py
@@ -1,17 +1,13 @@
-
 class Solution:
-
-    def sum_up(self, coins, pos, sum, amount):
-
-        cnt += 1
-        while sum + amount < pos:
-
-
-
-
-
 
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        self.m = [[-1 for i in range(amount)] for j in range(len(coins)))
-        return self.sum_up(coins, 0)
+        max_val = amount + 1
+        dp = [max_val for i in range(amount + 1)]
+        dp[0] = 0
+        for i in range(1, amount + 1):
+            for j in range(0, len(coins)):
+                if coins[j] <= i:
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+
+        return -1 if dp[amount] > amount else dp[amount];
